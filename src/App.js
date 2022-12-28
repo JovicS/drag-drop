0e42b886192583dfe3bbcb1882416c023bbb12a6
JovicS <jovic.sanja123@gmail.com>
2022-12-28 06:24:33 -0600

    Drag&Drop upgraded by adding react hook

--- src/App.js
+++ src/App.js
@@ -1,32 +1,14 @@
-import './App.css';
 import DragAndDropBoard from './dragAndDrop/DragAndDropBoard';
-import Card from './dragAndDrop/Card';
 
 function App() {
-  return (
-    <div className="flexbox">
-      <DragAndDropBoard id='board-1' className='board'>
-        <Card id='card-1' className='card' draggable='true'>
-          <p>ONE</p>
-        </Card>
-        <Card id='card-3' className='card' draggable='true'>
-          <h1>THREE</h1>
-        </Card>
-      </DragAndDropBoard>
+  
+  const data = [
+    {title: 'Group 1', items: ['1', '2', '3']},
+    {title: 'Group 2', items: ['4', '5']}
+  ]
 
-      <DragAndDropBoard id='board-2' className='board'>
-        <Card id='card-2' className='card' draggable='true'>
-          <p>TWO</p>
-        </Card>
-        <Card id='card-4' className='card' draggable='true'>
-          <div style={{color: 'red', fontSize: 'large'}}>
-            <span>
-              FOUR
-            </span>
-          </div>
-        </Card>
-      </DragAndDropBoard>
-    </div>
+  return (
+    <DragAndDropBoard data={data}/>
   );
 }
 
--- src/dragAndDrop/DragAndDropBoard.js
+++ src/dragAndDrop/DragAndDropBoard.js
@@ -1,26 +1,52 @@
-import React, { useRef, useState } from 'react'
+import React, { useRef, useState } from 'react';
+import '../index.css';
 
-function DragAndDropBoard(props) {
+function DragAndDropBoard({data}) {
 
-   const drop = (e) => {
-    e.preventDefault();
-    const card_id = e.dataTransfer.getData('card_id');
+  const [list, setList] = useState(data);
+  const dragItem = useRef();
+  const dragCard = useRef();
 
-    const card = document.getElementById(card_id);
-
-    e.target.appendChild(card);
+   const dragStart = (e, params) => {
+    // Dragged item
+    dragItem.current = params;
+    dragCard.current = e.target;
+    dragCard.current.addEventListener('dragend', dragEnd);
    }
 
-   const dragOver = (e) => {
-    e.preventDefault();
+   const dragEnd = () => {
+    dragCard.current.removeEventListener('dragend', dragEnd);
+    dragItem.current = null;
+    dragCard.current = null;
    }
 
+   const dragEnter = (e, params) => {
+    // Place to drag item
+      const currentItem = dragItem.current;
+      if(currentItem && e.target !== dragCard.current){
+        setList(oldList => {
+          let newList = JSON.parse(JSON.stringify(oldList));
+          newList[params.groupIndex].items.splice(params.itemIndex, 0, newList[currentItem.groupIndex].items.splice(currentItem.itemIndex,1)[0]);
+          dragItem.current = params;
+          return newList
+        })
+    }
+   }
+   
   return (
-    <div id={props.id}
-        className={props.className}
-        onDrop={drop}
-        onDragOver={dragOver}>
-        {props.children}
+    <div className='flexbox'>
+      {list?.map((group, groupIndex)=>
+        <div key={group.title} className='board' onDragEnter={!group.items?.length? (e) => {dragEnter(e,{groupIndex,itemIndex: 0})} : null}>
+          <div className='title'>{group.title}</div>
+          {group.items?.map((item, itemIndex)=>(
+            <div key={item} className={+item<3? 'non-draggable-card' : 'card'} draggable={+item>=3}
+                onDragStart={+item>=3? (e)=>dragStart(e, {groupIndex, itemIndex}) : null} 
+                onDragEnter={(e)=>dragEnter(e,{groupIndex, itemIndex})}>
+              {item}
+            </div>)
+          )}
+        </div>
+      )}
     </div>
   );
 }
